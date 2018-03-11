094c9cf7a3ab12a7b5ad96f7b59d8b13ad795afe
Farris <farris.ismati@gmail.com>
2018-03-11 14:10:20 -0400

    input to redux from user guess

--- src/components/play/UserInput.js
+++ src/components/play/UserInput.js
@@ -5,10 +5,16 @@ import React                          from 'react'
 import Button1                        from '../common/buttons/Button1'
 import Input1                         from '../common/inputs/Input1'
 
+const submitChat = (e, data, state) => {
+  e.preventDefault()
+  state.state.submitToChatlog({playerName: state.state.player.name, content: data})
+}
+
 const UserInput = (state) => (
   <div className="userinput__holder">
-    <Input1 className="input input__visible input__medium"/>
-    <Button1 click={()=>state.state.submitToChatlog({playerName: "Kevin", content: "lol"})} fontSize="2em" name="Ask" />
+    <Input1
+      onSubmit={(e)=>{submitChat(e, e.target[0].value, state); e.target[0].value = ""}}
+      className="input input__visible input__medium"/>
     <Button1 fontSize="2em" name="Guess" />
   </div>
 )
