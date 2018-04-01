10c4231fed228b87185282f76cb781b6642483eb
Farris <farris.ismati@gmail.com>
2018-04-01 16:32:13 -0400

    player connected notification

--- src/components/play/Play.js
+++ src/components/play/Play.js
@@ -10,22 +10,23 @@ import UserInput                      from './UserInput'
 class Play extends Component {
   constructor(){
     super()
-
+    this.state = {
+      socket: socketIOClient("localhost:3001")
+    }
     this.checkParity = this.checkParity.bind(this)
     this.submitChat = this.submitChat.bind(this)
   }
 
   //Checks parity of chat log to determine whether or not you can send a chat log
   checkParity() {
-    const length = this.props.game.chatLog.length
-    return length === 0 || length % 2 === 0 ? true : false
+    let length = this.props.game.chatLog.length - 1
+    return length <= 0 || length % 2 === 0 ? true : false
   }
 
   //Send Chat to Websockets
   submitChat (e, data, state) {
     e.preventDefault()
-    const socket = socketIOClient("localhost:3001")
-    socket.emit('send chat', {playerName: state.player.name, content: data, gameId: state.game.id})
+    this.state.socket.emit('send chat', {playerName: state.player.name, content: data, gameId: state.game.id})
   }
 
   componentDidMount() {
@@ -35,15 +36,23 @@ class Play extends Component {
       self.props.changeSetupPlayClass("play__holder ui__holder__active")
     }, 100)
 
+    if (!this.props.player.host && this.props.player.name && this.props.game.active) {
+      this.state.socket.emit('player connect', this.props.player.name)
+    }
+
     //RECIEVE CHAT FOR WEBSOCKETS
-    const socket = socketIOClient("localhost:3001");
-    socket.on('send chat', (data) => {
+    this.state.socket.on('send chat', (data) => {
+      console.log('CONNECTED  ')
       //Dispatch chat data to redux
       this.props.submitToChatlog({playerName: data.playerName, content: data.content})
       //Change guesses left
-      if (this.checkParity() && data.content.toLowerCase() != "idk" && this.props.game.guesses > 0){
+      if (this.checkParity() && data.content.toLowerCase() !== "idk" && this.props.game.guesses > 0){
         this.props.changeGuess(-1, this.props.player.host, this.props.game.id)
       }
+    })
+
+    this.state.socket.on('player connect', playerName => {
+      this.props.submitToChatlog({playerName: playerName, content: "Has connected!"})
     })
   }
 
